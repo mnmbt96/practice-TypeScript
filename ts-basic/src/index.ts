6e7737ec0957f2c80c3e70d8837fa6a9658b9559
Manami <vaattii1996@gmail.com>
2023-02-22 16:21:00 -0800

    Practice function

--- ts-basic/src/index.ts
+++ ts-basic/src/index.ts
@@ -5,8 +5,19 @@
 // world.sayHello(root);
 
 //03. Primitive Type
-import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+//04. Functions
+import { logMessage } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductPrice } from './function/parameters'
+logMessage('Hello TypeScript')
+isUserSignedIn('ABC', 'Manami')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+isUserSignedIn2('DEF')
+const sum = sumProductPrice(100, 200, 300, 400, 500)
+console.log('Function parameter sample5 : ', sum)
